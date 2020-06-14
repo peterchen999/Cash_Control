442017f17df74f88d4037d3aff14a23d195c28fc
Peter Chen <peter.chen999@gmail.com>
2020-06-14 23:36:47 +0800

    ver0.2，把所有的array資料包在class當中

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -10,7 +11,7 @@ class Entry{
         int date;
 
     public:
-        Enrty(){
+        Entry(){
         }
 
         ~Entry(){} 
@@ -33,7 +34,79 @@ class Entry{
         }
 };
 
-Entry New_Entry(){
+class Entry_Array{
+private:
+    Entry* operating_array;
+    int array_size;
+public:
+    Entry_Array(){
+        operating_array = new Entry[1000];
+        array_size = 0;
+    }
+    int get_array_size(void){
+        return array_size;
+    }
+    int get_amount(int id){
+        return operating_array[id].get_amount();
+    }
+    int get_month(int id){
+        return operating_array[id].get_month();
+    }
+    int get_date(int id){
+        return operating_array[id].get_date();
+    }
+    void add_entry(void){
+        int amount;
+        int month;
+        int date;
+        
+        cout<<"amount: ";
+        cin>>amount;
+        cout<<"month: ";
+        cin>>month;
+        cout<<"date: ";
+        cin>>date;
+    
+        Entry new_entry;
+        new_entry.Set_Entry(amount, month, date);
+        //set up the correct entry data
+
+        if (array_size == 0){
+            array_size++;
+            operating_array[0] = new_entry;
+        }
+        else{
+            int i;
+            int new_entry_date_sum = new_entry.get_month()*31+new_entry.get_date();
+            //find the approximate no. of day of the year
+
+            for (i = 0; i < array_size; i++){
+                int date_sum = operating_array[i].get_month()*31+operating_array[i].get_date();
+                //find the approximate no. of day of the year
+
+                if (new_entry_date_sum == date_sum){
+                    i++;
+                    break;
+                }
+                else if (new_entry_date_sum < date_sum){
+                    break;
+                }
+            }
+            // find the appropriate place to insert new entry
+
+            array_size++;
+
+            for (int k = array_size-1; k > i; k--){
+                operating_array[k] = operating_array[k-1];
+            }
+            operating_array[i] = new_entry;
+        
+        }
+    }
+
+};
+
+/*Entry New_Entry(){
     int amount;
     int month;
     int date;
@@ -48,9 +121,10 @@ Entry New_Entry(){
     new_entry.Set_Entry(amount, month, date);
 
     return new_entry;
-}
+}*/
+//function is been replaced by "add_entry()" in Entry_Array Class
 
-void Add_Entry_To_Array(Entry* operating_array, int &array_size, Entry new_entry){
+/*void Add_Entry_To_Array(Entry* operating_array, int &array_size, Entry new_entry){
     if (operating_array == NULL){
         operating_array[0] = new_entry;
     }
@@ -83,27 +157,43 @@ void Add_Entry_To_Array(Entry* operating_array, int &array_size, Entry new_entry
     }
 
     
-}
+}*/
+//function is been replaced by "add_entry()" in Entry_Array Class
 
-void Print_All_Entry(Entry *operating_array, int array_size){
-    for (int i = 0; i < array_size; i++){
+void Print_All_Entry(Entry_Array input){
+    for (int i = 0; i < input.get_array_size(); i++){
         cout<<"Entry no."<<i<<endl;
-        cout<<"amount = "<<operating_array[i].get_amount()<<endl;
-        cout<<"month = "<<operating_array[i].get_month()<<endl;
-        cout<<"date = "<<operating_array[i].get_date()<<endl;
+        cout<<"amount = "<<input.get_amount(i)<<endl;
+        cout<<"month = "<<input.get_month(i)<<endl;
+        cout<<"date = "<<input.get_date(i)<<endl;
     }
 }
 
-int main(void){
-    Entry operating_array[1000];
-    operating_array[0] = New_Entry();
-    operating_array[1] = New_Entry();
-    operating_array[2] = New_Entry();
+void Save_To_Text(Entry_Array input){
+    std :: fstream ouput_text("result.txt", std :: fstream :: out);
+    for (int i = 0; i < input.get_array_size(); i++){
+        ouput_text<<i<<endl;
+        ouput_text<<input.get_amount(i)<<endl;
+        ouput_text<<input.get_month(i)<<endl;
+        ouput_text<<input.get_date(i)<<endl;
+    }
+    ouput_text.close();
+}
 
-    int array_size = 3;
-    Add_Entry_To_Array(operating_array, array_size, New_Entry());
-    Print_All_Entry(operating_array, 4);
+
+
+int main(void){
+    Entry_Array data;
+    //initialize the array
+    //int array_size = 0;
+    data.add_entry();
+    data.add_entry();
+    data.add_entry();
+    data.add_entry();
+
+    Print_All_Entry(data);
+    Save_To_Text(data);
     char wait;
-    cin>>wait;
+    cin>>wait;  
     return 0;
 }
